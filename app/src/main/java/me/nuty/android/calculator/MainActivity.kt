ceadbc417cdfdad12c6cb5e5100a571d2ab30684
nutyworks <nutyworks@gmail.com>
2020-05-13 17:39:22 +0900

    use synthetic

--- app/src/main/java/me/nuty/android/calculator/MainActivity.kt
+++ app/src/main/java/me/nuty/android/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,26 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnAdd = findViewById<Button>(R.id.btn_add)
-        val btnSub = findViewById<Button>(R.id.btn_sub)
-        val btnMul = findViewById<Button>(R.id.btn_mul)
-        val btnDiv = findViewById<Button>(R.id.btn_div)
-
-        val firstNum = findViewById<EditText>(R.id.first_num)
-        val secondNum = findViewById<EditText>(R.id.second_num)
-        val resultNum = findViewById<TextView>(R.id.result_num)
-
-        btnAdd.setOnClickListener {
-            resultNum.text = calculate(firstNum, secondNum, Operation.ADD).toString()
+        btn_add.setOnClickListener {
+            result_num.text = calculate(first_num, second_num, Operation.ADD).toString()
         }
-        btnSub.setOnClickListener {
-            resultNum.text = calculate(firstNum, secondNum, Operation.SUBTRACT).toString()
+        btn_sub.setOnClickListener {
+            result_num.text = calculate(first_num, second_num, Operation.SUBTRACT).toString()
         }
-        btnMul.setOnClickListener {
-            resultNum.text = calculate(firstNum, secondNum, Operation.MULTIPLY).toString()
+        btn_mul.setOnClickListener {
+            result_num.text = calculate(first_num, second_num, Operation.MULTIPLY).toString()
         }
-        btnDiv.setOnClickListener {
-            resultNum.text = calculate(firstNum, secondNum, Operation.DIVIDE).toString()
+        btn_mul.setOnClickListener {
+            result_num.text = calculate(first_num, second_num, Operation.DIVIDE).toString()
         }
     }
 
@@ -44,7 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     enum class Operation(val evaluate: (a: Int, b: Int) -> Any) {
         ADD({ a, b -> a + b }),
-        SUBTRACT( {a, b -> a - b }),
+        SUBTRACT({ a, b -> a - b }),
         MULTIPLY({ a, b -> a * b }),
         DIVIDE({ a, b ->
             if (b == 0) "0으로 나누기"
